b271fe0d71a389aa1105fd5d90710137a90de275
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2021-09-13 19:19:22 +0300

    less 78 final

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,8 @@ import is from 'is_js'
 export default class Auth extends Component {
 
     state ={
+
+        isFormValid:false,
         formControls : {
             email : {
                 value: '',
@@ -81,8 +83,13 @@ export default class Auth extends Component {
         // Обновляем локальную fromControls
         formControls[controlName] = control
 
+        let isFormValid = true
+        Object.keys(formControls).forEach(name=>{
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
 
     }
@@ -121,12 +128,16 @@ export default class Auth extends Component {
 
                         <Button
                             type='success'
-                            onClick={this.loginHandler}>
+                            onClick={this.loginHandler}
+                            disabled = {!this.state.isFormValid}
+                            >
                             Войти</Button>
 
                         <Button
                             type='success'
-                            onClick={this.registerHandler}>
+                            onClick={this.registerHandler}
+                            disabled = {!this.state.isFormValid}
+                            >
                             Зарегистрироваться</Button>
                     </form>
                 </div>
