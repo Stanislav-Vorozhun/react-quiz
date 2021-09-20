684588cae975b6bade1382fefc242e20c3f39182
Stanislav-Vorozhun <stanislav.vorozhun@gmail.com>
2021-09-21 01:23:56 +0300

    less 81 final

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import classes from "./QuizCreator.module.css"
 import Button from "../../components/ActiveQuiz/UI/Button/Button"
 import Input from "../../components/ActiveQuiz/UI/Input/Input"
+import Select from "../../components/ActiveQuiz/UI/Select/Select"
 import { createControl } from "../../form/formFramework"
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary"
 
@@ -14,7 +15,7 @@ function createOptionControl(number) {
     }, { required: true })
 }
 
-function createFormControls () {
+function createFormControls() {
     return {
         question: createControl({
             label: 'Введите вопрос',
@@ -31,7 +32,8 @@ export default class QuizCreator extends Component {
 
     state = {
         quiz: [],
-        formControls: createFormControls()
+        formControls: createFormControls(),
+        rightAnswerId: 1
     }
 
     submitHandler = event => {
@@ -46,33 +48,51 @@ export default class QuizCreator extends Component {
 
     }
 
-    changeHandler=(value, controlName)=>{
+    changeHandler = (value, controlName) => {
 
     }
 
-    renderControls () {
-        return Object.keys(this.state.formControls).map((controlName, index)=>{
+    renderControls() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
             const control = this.state.formControls[controlName]
-                return(
-                    <Auxiliary key={controlName+index}>
-                        <Input
+            return (
+                <Auxiliary key={controlName + index}>
+                    <Input
                         label={control.label}
                         value={control.value}
                         valid={control.valid}
                         shouldValidate={!!control.validation}
                         touched={control.touched}
                         errorMessage={control.errorMessage}
-                        onChange={event=>this.changeHandler(event.target.value, controlName)}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
                     />
-                    {index === 0 ? <hr />:null}
-                    </Auxiliary>
-                    
+                    {index === 0 ? <hr /> : null}
+                </Auxiliary>
 
-                )
+
+            )
+        })
+    }
+
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId:+event.target.value
         })
     }
 
     render() {
+        const select = <Select
+            label='Выберите правильный ответ'
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                { text: 1, value: 1 },
+                { text: 2, value: 2 },
+                { text: 3, value: 3 },
+                { text: 4, value: 4 }
+            ]
+            }
+        />
         return (
             <div className={classes.QuizCreator}>
                 <div>
@@ -82,7 +102,7 @@ export default class QuizCreator extends Component {
                         {this.renderControls()}
 
 
-                        <select></select>
+                        {select}
                         <Button
                             type='primary'
                             onClick={this.addQuestionHandler}
