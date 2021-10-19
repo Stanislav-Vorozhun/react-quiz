3ee14f131e1527824be2b31e00afc7d3d787f7b3
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2021-10-19 17:58:35 +0300

    less 104 final

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -4,6 +4,10 @@ import {
     FETCH_QUIZES_START,
     FETCH_QUIZES_SUCCESS,
     FETCH_QUIZE_SUCCESS,
+    QUIZ_SET_STATE,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    RETRY_QUIZ
 } from './actionTypes'
 
 
@@ -70,4 +74,72 @@ export function fetchQuizesError(e) {
         error: e
     }
 
+}
+
+export function quizSetState(answerState, results){
+    return {
+        type: QUIZ_SET_STATE,
+        answerState, results
+    }
+}
+
+export function finishQuiz(){
+    return{
+        type: FINISH_QUIZ
+    }
+}
+
+export function quizNextQuestion(number){
+    return {
+        type: QUIZ_NEXT_QUESTION,
+        number
+    }
+}
+
+export function quizAnswerClick(answerId) {
+    return (dispatch, getState) => {
+        const state = getState().quiz
+        // фикс бага с двоим кликом по правильному ответу (пропуск вопроса)
+        if (state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (state.answerState[key] === 'success') {
+                return
+            }
+        }
+
+        const question = state.quiz[state.activeQuestion]
+        const results = state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
+            dispatch(quizSetState({[answerId]: 'success'}, results)) 
+
+            const timeout = window.setTimeout(() => {
+                if (isQuizFinished(state)) {
+                    dispatch(finishQuiz())
+                } else {
+                    dispatch(quizNextQuestion(state.activeQuestion + 1))
+                }
+                window.clearTimeout(timeout)
+            }, 1000)
+
+        } else {
+            results[question.id] = 'error'
+            dispatch(quizSetState({[answerId]: 'error'}, results))
+
+        }
+
+    }
+}
+
+export function retryQuiz(){
+    return {
+        type: RETRY_QUIZ
+    }
+}
+
+function isQuizFinished (state) {
+    return state.activeQuestion + 1 === state.quiz.length
 }
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -2,7 +2,11 @@ import {
     FETCH_QUIZES_ERROR,
     FETCH_QUIZES_START,
     FETCH_QUIZES_SUCCESS,
-    FETCH_QUIZE_SUCCESS
+    FETCH_QUIZE_SUCCESS,
+    QUIZ_SET_STATE,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    RETRY_QUIZ
 } from "../actions/actionTypes"
 
 const initialState = {
@@ -33,7 +37,27 @@ export default function quizReducer(state = initialState, action) {
             }
         case FETCH_QUIZE_SUCCESS:
             return {
-                ...state,loading: false, quiz:action.quiz
+                ...state, loading: false, quiz: action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state, answerState: action.answerState, result: action.result
+            }
+        case FINISH_QUIZ:
+            return {
+                ...state, isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state, answerState: null, activeQuestion: action.number
+            }
+        case RETRY_QUIZ:
+            return {
+                ...state,
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             }
         default:
             return state
